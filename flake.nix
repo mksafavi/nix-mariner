3501bd4f5a92eafe5592cb9ef59e546a11805607
mksafavi <mksafavi@gmail.com>
2026-06-29 22:14:59 +0330

    Add vm skeleton

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.microvm.url = "github:microvm-nix/microvm.nix";
+  inputs.microvm.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      microvm,
+    }:
+    {
+      nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          microvm.nixosModules.microvm
+          {
+            networking.hostName = "vm";
+            microvm.hypervisor = "qemu";
+          }
+        ];
+      };
+    };
+}
