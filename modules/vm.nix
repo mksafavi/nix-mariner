63c1e7b3a1d08f7451d8a949ccd8989fc3e99d8a
mksafavi <mksafavi@gmail.com>
2026-06-30 01:20:13 +0330

    Mount host nix store as readonly

--- modules/vm.nix
+++ modules/vm.nix
@@ -7,4 +7,12 @@
       mac = "02:00:00:00:00:01";
     }
   ];
+
+  microvm.shares = [
+    {
+      tag = "ro-store";
+      source = "/nix/store";
+      mountPoint = "/nix/.ro-store";
+    }
+  ];
 }
