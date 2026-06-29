952f9d06b5043e48cbab377dcd4a70ef370be773
mksafavi <mksafavi@gmail.com>
2026-06-30 01:37:39 +0330

    Add network interface eth0

--- modules/vm.nix
+++ modules/vm.nix
@@ -1,5 +1,7 @@
 { config, ... }:
 {
+  systemd.network.enable = true;
+
   microvm.interfaces = [
     {
       type = "tap";
@@ -7,6 +9,15 @@
       mac = "02:00:00:00:00:01";
     }
   ];
+
+  systemd.network.networks."10-lan" = {
+    matchConfig.Type = "ether";
+    networkConfig = {
+      Address = [ "10.0.0.2/24" ];
+      Gateway = "10.0.0.1";
+      DNS = [ "10.0.0.1" ];
+    };
+  };
 
   microvm.shares = [
     {
