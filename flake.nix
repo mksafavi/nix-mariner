7d65d6d7693484cc7c69461d2b4f842c0b0755a7
mksafavi <mksafavi@gmail.com>
2026-06-30 20:03:02 +0330

    Separate network module

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
           modules/common.nix
           modules/vm.nix
           modules/storage.nix
+          modules/network.nix
         ];
       };
     };
--- modules/vm.nix
+++ modules/vm.nix
@@ -1,27 +1,8 @@
 { config, ... }:
 {
-  microvm.hypervisor = "qemu";
-
   networking.hostName = "vm";
 
-  systemd.network.enable = true;
-
-  microvm.interfaces = [
-    {
-      type = "tap";
-      id = "vm-${config.networking.hostName}";
-      mac = "02:00:00:00:00:01";
-    }
-  ];
-
-  systemd.network.networks."10-lan" = {
-    matchConfig.Type = "ether";
-    networkConfig = {
-      Address = [ "10.0.0.2/24" ];
-      Gateway = "10.0.0.1";
-      DNS = [ "10.0.0.1" ];
-    };
-  };
+  microvm.hypervisor = "qemu";
 
   microvm = {
     vcpu = 4;
