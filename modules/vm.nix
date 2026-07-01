c0a9646fde89d603bb3c209c7d16ef4ad3260d72
mksafavi <mksafavi@gmail.com>
2026-07-01 22:51:59 +0330

    Add hostName option and default it to username

--- modules/vm.nix
+++ modules/vm.nix
@@ -9,8 +9,14 @@
     description = "VSOCK context ID. Must be >= 3 and unique per host";
   };
 
+  options.mariner.hostName = lib.mkOption {
+    type = lib.types.str;
+    default = config.mariner.username;
+    description = "The name of the machine";
+  };
+
   config = {
-    networking.hostName = lib.mkDefault "vm";
+    networking.hostName = config.mariner.hostName;
 
     microvm.hypervisor = lib.mkDefault "qemu";
 
