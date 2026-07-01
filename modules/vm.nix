bf25cc27efd5afac21e29c6ec78e53fce15e9a8b
mksafavi <mksafavi@gmail.com>
2026-07-02 00:42:52 +0330

    Remove hostName option

--- modules/vm.nix
+++ modules/vm.nix
@@ -9,15 +9,7 @@
     description = "VSOCK context ID. Must be >= 3 and unique per host";
   };
 
-  options.mariner.hostName = lib.mkOption {
-    type = lib.types.str;
-    default = config.mariner.username;
-    description = "The name of the machine";
-  };
-
   config = {
-    networking.hostName = config.mariner.hostName;
-
     microvm.hypervisor = lib.mkDefault "qemu";
 
     microvm.vcpu = lib.mkDefault 4;
