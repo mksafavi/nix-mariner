3480d7d5e2c375f9b8bc6cb26892b77110f2f5b8
mksafavi <mksafavi@gmail.com>
2026-07-11 06:41:28 +0330

    Add empty distrobox settings option

--- modules/distrobox.nix
+++ modules/distrobox.nix
@@ -4,9 +4,20 @@
   pkgs,
   ...
 }:
+let
+  cfg = config.mariner.distrobox;
+  settingsFormat = pkgs.formats.ini { listsAsDuplicateKeys = true; };
+  settingsFile = settingsFormat.generate "distrobox.ini" cfg.settings;
+in
 {
   options.mariner.distrobox = {
     enable = lib.mkEnableOption "distrobox integration";
+
+    settings = lib.mkOption {
+      type = settingsFormat.type;
+      default = { };
+      description = "Configuration for distrobox manifest";
+    };
   };
 
   config = lib.mkIf config.mariner.distrobox.enable {
