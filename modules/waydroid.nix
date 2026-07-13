a4fb6f8ef156f62d15569d96eb5ad39f1b04139a
mksafavi <mksafavi@gmail.com>
2026-07-14 02:48:24 +0330

    Add waydroid init service

--- modules/waydroid.nix
+++ modules/waydroid.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -14,5 +15,26 @@ in
   config = lib.mkIf cfg.enable {
 
     virtualisation.waydroid.enable = true;
+
+    systemd.services.waydroid-init = {
+      description = "initialize waydroid";
+      wantedBy = [ "multi-user.target" ];
+      after = [
+        "network-online.target"
+      ];
+      wants = [
+        "network-online.target"
+      ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        Restart = "on-failure";
+        RestartSec = "10s";
+      };
+      script = ''
+        ${pkgs.waydroid}/bin/waydroid init
+      '';
+    };
   };
 }
