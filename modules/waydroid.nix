399f6aa297a8c6322ff3859e40e066fa90cb1ab9
mksafavi <mksafavi@gmail.com>
2026-07-15 01:59:17 +0330

    Add systemd service for starting waydroid session

--- modules/waydroid.nix
+++ modules/waydroid.nix
@@ -48,5 +48,31 @@ in
         ${pkgs.waydroid}/bin/waydroid init -s ${cfg.systemImage}
       '';
     };
+
+    systemd.services.waydroid-session-start = {
+      description = "start waydroid session inside a waypipe server";
+      wantedBy = [ "multi-user.target" ];
+      after = [
+        "waydroid-init.service"
+        "waydroid-container.service"
+      ];
+      requires = [
+        "waydroid-init.service"
+        "waydroid-container.service"
+      ];
+      path = [
+        pkgs.dbus
+      ];
+      serviceConfig = {
+        Restart = "always";
+        RestartSec = "10s";
+        User = config.mariner.username;
+      };
+      script = ''
+        ${pkgs.dbus}/bin/dbus-run-session \
+        ${pkgs.waypipe}/bin/waypipe --vsock -s 2:6000 server \
+        ${pkgs.waydroid}/bin/waydroid session start
+      '';
+    };
   };
 }
