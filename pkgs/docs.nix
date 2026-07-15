1ae2d1ca7c95c77eeb99a12183001f52de6b6e46
mksafavi <mksafavi@gmail.com>
2026-07-16 01:06:44 +0330

    Split makeOptionsDoc function

--- pkgs/docs.nix
+++ pkgs/docs.nix
@@ -11,49 +11,50 @@ let
     else
       "${sourceInfo.repo}/tree/${sourceInfo.ref}";
 
+  evalModulesForDoc =
+    modules:
+    lib.evalModules {
+      specialArgs = { inherit pkgs; };
+      modules = modules ++ [
+        ({ lib, ... }: {
+          config._module.check = false; # Skip NixOS module checks
+          options._module.args = lib.mkOption {
+            internal = true; # Hide NixOS `_module.args` from nixosOptionsDoc
+          };
+        })
+      ];
+    };
+
+  transformOptions =
+    opt:
+    opt
+    // {
+      declarations = map (
+        decl:
+        let
+          root = toString ../.;
+          declStr = toString decl;
+          declPath = lib.removePrefix root decl;
+        in
+        if lib.hasPrefix root declStr then
+          {
+            name = declPath;
+            url = "${url}${declPath}";
+          }
+        else
+          decl
+      ) opt.declarations;
+    };
+
   makeOptionsDoc =
     modules:
     pkgs.nixosOptionsDoc {
-      inherit
-        (
-          (lib.evalModules {
-            specialArgs = { inherit pkgs; };
-            modules = modules ++ [
-              ({ lib, ... }: {
-                config._module.check = false; # Skip NixOS module checks
-                options._module.args = lib.mkOption {
-                  internal = true; # Hide NixOS `_module.args` from nixosOptionsDoc
-                };
-              })
-            ];
-          })
-        )
-        options
-        ;
-      transformOptions =
-        opt:
-        opt
-        // {
-          declarations = map (
-            decl:
-            let
-              root = toString ../.;
-              declStr = toString decl;
-              declPath = lib.removePrefix root decl;
-            in
-            if lib.hasPrefix root declStr then
-              {
-                name = declPath;
-                url = "${url}${declPath}";
-              }
-            else
-              decl
-          ) opt.declarations;
-        };
+      inherit (evalModulesForDoc modules) options;
+      inherit transformOptions;
     };
 
-  modulesOptionsDocs = makeOptionsDoc ([ ../modules ]);
-  hostOptionsDocs = makeOptionsDoc ([ ../modules/host.nix ]);
+  modulesOptionsDoc = makeOptionsDoc ([ ../modules ]);
+  hostOptionsDoc = makeOptionsDoc ([ ../modules/host.nix ]);
 in
 pkgs.runCommand "nix-mariner-docs"
   {
@@ -62,7 +63,7 @@ pkgs.runCommand "nix-mariner-docs"
   ''
     cp -Lr ${../docs} docs
     chmod u+w docs/src
-    cp ${modulesOptionsDocs.optionsCommonMark} docs/src/mariner-options.md
-    cp ${hostOptionsDocs.optionsCommonMark} docs/src/mariner-host-options.md
+    cp ${modulesOptionsDoc.optionsCommonMark} docs/src/mariner-options.md
+    cp ${hostOptionsDoc.optionsCommonMark} docs/src/mariner-host-options.md
     mdbook build -d $out docs
   ''
