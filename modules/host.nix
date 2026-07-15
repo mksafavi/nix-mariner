39c7ca46b92a4d606297c439c820b060ab60d557
mksafavi <mksafavi@gmail.com>
2026-07-16 01:39:16 +0330

    Add the host user service for starting the waypipe client

--- modules/host.nix
+++ modules/host.nix
@@ -18,5 +18,19 @@ in
 
   config = lib.mkIf cfg.enable {
     microvm.host.enable = true;
+
+    systemd.user.services.mariner-waypipe-client = {
+      description = "Start waypipe client";
+      wantedBy = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      serviceConfig = {
+        Restart = "always";
+        RestartSec = "10s";
+      };
+      script = ''
+        ${pkgs.waypipe}/bin/waypipe --vsock -s 2:6000 client
+      '';
+    };
   };
 }
