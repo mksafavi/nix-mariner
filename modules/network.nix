dd784b5b689f91b39c504eab4c278c58755243ba
mksafavi <mksafavi@gmail.com>
2026-07-18 18:13:31 +0330

    Add mariner.network.gateway option to set gateway and dns address

--- modules/network.nix
+++ modules/network.nix
@@ -3,21 +3,32 @@
   lib,
   ...
 }:
+let
+  cfg = config.mariner.network;
+in
 {
-  options.mariner.network.address = lib.mkOption {
-    type = lib.types.str;
-    default = "10.0.0.${toString config.mariner.cid}/24";
-    defaultText = "Derived from `mariner.cid`";
-    description = "Static IPv4 address assigned to the LAN interface.";
-  };
+  options.mariner.network = {
+    address = lib.mkOption {
+      type = lib.types.str;
+      default = "10.0.0.${toString config.mariner.cid}/24";
+      defaultText = "Derived from `mariner.cid`";
+      description = "Static IPv4 address assigned to the LAN interface.";
+    };
 
-  options.mariner.network.mac = lib.mkOption {
-    type = lib.types.str;
-    default = "02:00:00:00:00:${
-      lib.fixedWidthString 2 "0" (lib.toLower (lib.toHexString config.mariner.cid))
-    }";
-    defaultText = "Derived from `mariner.cid`";
-    description = "MAC address for the LAN interface.";
+    gateway = lib.mkOption {
+      type = lib.types.str;
+      default = "10.0.0.1";
+      description = "Host static IPv4 address on the bridge.";
+    };
+
+    mac = lib.mkOption {
+      type = lib.types.str;
+      default = "02:00:00:00:00:${
+        lib.fixedWidthString 2 "0" (lib.toLower (lib.toHexString config.mariner.cid))
+      }";
+      defaultText = "Derived from `mariner.cid`";
+      description = "MAC address for the LAN interface.";
+    };
   };
 
   config = {
@@ -34,11 +45,11 @@
     ];
 
     systemd.network.networks."10-lan" = lib.mkDefault {
-      matchConfig.MACAddress = config.mariner.network.mac;
+      matchConfig.MACAddress = cfg.mac;
       networkConfig = {
-        Address = [ config.mariner.network.address ];
-        Gateway = "10.0.0.1";
-        DNS = [ "10.0.0.1" ];
+        Address = [ cfg.address ];
+        Gateway = cfg.gateway;
+        DNS = [ cfg.gateway ];
       };
     };
   };
