f8d427a47a7e1c4100fb4ca831ee2a619d64cb75
mksafavi <mksafavi@gmail.com>
2026-08-03 02:57:41 +0330

    Enable vulkan venus backend on graphical example

--- examples/graphical.nix
+++ examples/graphical.nix
@@ -3,6 +3,8 @@
   mariner.cid = 6;
   mariner.ssh.authorizedKeys = [ "ssh-ed25519 AAAA... user@host" ];
   mariner.graphics.enable = true;
+  microvm.graphics.vulkan = "venus";
+  microvm.graphics.hostmem = "8G";
   environment.systemPackages = with pkgs; [
     firefox
   ];
